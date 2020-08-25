f53dfb0d584335a4a862420fab9b48d3494c8f65
Jens Johnston <johnstonjr27@gmail.com>
2020-08-25 20:14:02 +0200

    Dark-mode not working

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,23 +11,21 @@ $(document).ready(() => {
 
 const mqDark = window.matchMedia("(prefers-color-scheme: dark)")
 
-
 const darkModeToggle = document.querySelector("a.dark-mode-toggle")
-const darkModeToggleText = darkModeToggle.querySelector("span")
+const darkModeToggleText = document.querySelector("span")
 
-const bodyTag = document.querySelector("body")
+const bodyTag = document.querySelector('body')
 
-darkModeToggleText.addEventListener("click", function () {
-    bodyTag.classList.toggle("dark-mode")
+darkModeToggle.addEventListener("click", function () {
+    bodyTag.classList.toggle("dark-mode");
 })
+
 
 const updateDarkMode = function () {
     if (mqDark.matches) {
-        bodyTag.classList.add("dark-mode")
-        darkModeToggleText.innerHTML = "Light mode"
+        darkModeToggle.innerHTML= "Light mode"
     } else {
-        bodyTag.classList.remove("dark-mode")
-        darkModeToggleText.innerHTML = "Dark mode"
+        darkModeToggle.innerHTML = "Dark mode"
     }
 }
 
@@ -35,4 +33,3 @@ updateDarkMode()
 mqDark.addListener(function () {
     updateDarkMode()
 })
-
