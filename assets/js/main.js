713696a9c4eff7d941ad3c3086c883717b8d7f5e
Jens Johnston <johnstonjr27@gmail.com>
2021-05-02 18:09:17 +0200

    add new images

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,33 +24,22 @@ const questions = document.querySelectorAll('section')
 const lightbox = document.querySelector('.lightbox')
 
 questions.forEach((question) => {
+    const backDrop = document.querySelector('div.lightbox-background')
+    const modalClose = document.querySelector('div.lightbox-background')
     const opener = question.querySelector('.image') 
+
         opener.addEventListener('click', () => {
             [...questions].filter(q => q !== question).forEach(q => q.classList.remove('opened'))
             question.classList.toggle('opened')
         })
-})
 
-const slides = document.querySelector(".slideshow")
-const pagination = document.querySelectorAll(".pagination-circle")
-
-let current = 0
-let z = 10000000
-
-
-const slideshow = slides.addEventListener("click", function() {
-    z = z - 1
-
-    images[current].style.zIndex = z
-
-    current = current + 1
-    current = current % images.length
-
+        modalClose.addEventListener('click', () => {
+            [...questions].filter(q => q !== question).forEach(q => q.classList.remove('opened'))
+            question.classList.remove('opened')
+        })
 })
 
 
-pagination.addEventListener("click", function() {
-    
-})
+
 
 
